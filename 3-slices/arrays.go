76adba45de9719d6140f5a70cc4206316b1bcf2a
Giovani Moutinho <giovanim@ymail.com>
2021-10-29 10:08:55 -0300

    Add sumAllTails function

--- 3-slices/arrays.go
+++ 3-slices/arrays.go
@@ -17,3 +17,12 @@ func sumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func sumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, sum(tail))
+	}
+	return sums
+}
--- 3-slices/arrays_test.go
+++ 3-slices/arrays_test.go
@@ -3,7 +3,7 @@ package main
 import "reflect"
 import "testing"
 
-func TestSum(t *testing.T) {
+func testSum(t *testing.T) {
 	numbers := []int{1, 2, 3}
 
 	got := sum(numbers)
@@ -14,9 +14,18 @@ func TestSum(t *testing.T) {
 	}
 }
 
-func TestSumAll(t *testing.T) {
+func testSumAll(t *testing.T) {
 	got := sumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func testSumAllTails(t *testing.T) {
+	got := sumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
