22e9d4afb965b18c73808233085edbded8af069d
Giovani Moutinho <giovanim@ymail.com>
2021-10-29 09:58:06 -0300

    Create sumAll function

--- 3-slices/arrays.go
+++ 3-slices/arrays.go
@@ -1,9 +1,20 @@
 package main
 
 func sum(numbers []int) int {
-    sum := 0
-    for _, number := range numbers {
-        sum += number
-    }
-    return sum
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func sumAll(numbersToSum ...[]int) []int {
+	lenghtOfNumbers := len(numbersToSum)
+	sums := make([]int, lenghtOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = sum(numbers)
+	}
+
+	return sums
 }
--- 3-slices/arrays_test.go
+++ 3-slices/arrays_test.go
@@ -1,16 +1,26 @@
 package main
 
+import "reflect"
 import "testing"
 
 func TestSum(t *testing.T) {
-    t.Run("collection of any size", func(t *testing.T) {
-        numbers := []int{1, 2, 3}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-        got := sum(numbers)
-        expected := 6
+		got := sum(numbers)
+		expected := 6
 
-        if got != expected {
-            t.Errorf("got %d, expected %d for %v", got, expected, numbers)
-        }
-    })
+		if got != expected {
+			t.Errorf("got %d, expected %d for %v", got, expected, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := sumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
