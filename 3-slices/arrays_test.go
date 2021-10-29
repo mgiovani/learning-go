d0a98c2fcd4a9e759e890ccbf5f1c7bf34e575bb
Giovani Moutinho <giovanim@ymail.com>
2021-10-29 10:21:03 -0300

    Refactor sumAllTails tests

--- 3-slices/arrays_test.go
+++ 3-slices/arrays_test.go
@@ -24,21 +24,24 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
+    checkSums := func(t testing.TB, got []int, want []int) {
+        t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+    }
+
 	t.Run("return sums of slices", func(t *testing.T) {
 		got := sumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+        checkSums(t, got, want)
 	})
 
 	t.Run("return sums for empty slices", func(t *testing.T) {
 		got := sumAllTails([]int{}, []int{3, 4, 5})
         want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+        checkSums(t, got, want)
 	})
 }
