2dad85fb5ac6db2d43dd892bad06cddb61af4667
Giovani Moutinho <giovanim@ymail.com>
2021-10-29 09:50:02 -0300

    Refactor slices tests

--- 3-slices/arrays_test.go
+++ 3-slices/arrays_test.go
@@ -3,11 +3,11 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    t.Run("collection of 5 numbers", func(t *testing.T) {
-        numbers := []int{1, 2, 3, 4, 5}
+    t.Run("collection of any size", func(t *testing.T) {
+        numbers := []int{1, 2, 3}
 
         got := sum(numbers)
-        expected := 15
+        expected := 6
 
         if got != expected {
             t.Errorf("got %d, expected %d for %v", got, expected, numbers)
